f43136c544f1f1f3cb578b3b659eab56e58f302d
21506934 <21506934@etu.unicaen.fr>
2016-05-10 22:44:56 +0200

    Revert "TODO 1.3 and TODO 1.4 : create and update disk"
    This reverts commit 84da3790e02948937df5bca1c69c0de6662e96b7.

--- app/controllers/Disques.php
+++ app/controllers/Disques.php
@@ -1,66 +1,9 @@
 <?php
-use micro\orm\DAO;
 class Disques extends \_DefaultController {
 
 	public function __construct(){
 		parent::__construct();
 		$this->title="Disques";
 		$this->model="Disque";
-	}
-
-
-	public function frm($id=NULL){
-		$disque=$this->getInstance($id);
-		if($disque->getUtilisateur() != NULL) {
-			$idUtilisateur = $disque->getUtilisateur()->getId();
-		} else {
-			$idUtilisateur = Auth::getUser()->getId();
-            $disque = new stdClass();
-			$disque->id = 0;
-			$disque->nom = "";
-		}
-		$this->loadView("disques/vAdd.html",array("disque"=>$disque, 'idUtilisateur'=>$idUtilisateur));
-	}
-
-	public function update(){
-		// Si un ID et un nom sont passés en paramètres, il s'agit de mettre à jour un disque ***
-		if($_POST["id"] && $_POST['nom']) {
-			// On recupère le chemin ABSOLU du dossier (disque) grace à l'ancien nom du disque disque et au variable globale
-			$oldfolder = DAO::getOne('Disque', $_POST['id'])->getNom();
-			$basepath = (dirname(dirname(__DIR__))."/files/".$GLOBALS['config']['cloud']['prefix'].Auth::getUser()->getLogin().'/');
-			$actualpath = $basepath.$oldfolder;
-			$newpath = $basepath.$_POST['nom'];
-			// Ensuite une exception classique pour tester si tout s'est bien passé !
-			try {
-				rename($actualpath, $newpath);
-			} catch (Exception $e) {
-				die("Erreur pour renommer le dossier");   
-			}
-		// *** Sinon, il s'agit de créer un disque
-		} else {
-			if ($_POST['nom']) {
-				// On recupère le chemin ABSOLU du dossier (disque) comme au dessus
-				$basepath = (dirname(dirname(__DIR__))."/files/".$GLOBALS['config']['cloud']['prefix'].Auth::getUser()->getLogin().'/');
-				$newpath = $basepath.$_POST['nom'];
-				// Ensuite une exception classique pour tester si la création a fonctionné !
-				try {
-					mkdir($newpath);
-				} catch (Exception $e) {
-					die("Erreur de créer le dossier");   
-				}
-			}
-		}
-		// On appelle ensuite la fonction update du DefaultController pour mettre à jour les paramètres en base de données.
-		parent::update();
-	}
-
-	// Réecriture de la fonction parente
-	// On "set" l'objet utilisateur dans l'objet disque afin de pouvoir utiliser la fonction toString de Disque
-	protected function setValuesToObject(&$object) {
-		parent::setValuesToObject($object);
-		if(isset($_POST["idUtilisateur"])) {
-			$user = DAO::getOne('Utilisateur', $_POST["idUtilisateur"]);
-			$object->setUtilisateur($user);
-		}
 	}
 }
