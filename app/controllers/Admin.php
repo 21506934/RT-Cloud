dc224b2eb94829b49f8e83601a338d15c5bd5c81
21503241 <21503241@etu.unicaen.fr>
2016-05-10 23:01:15 +0200

    TODO 2.2 : Ajout de la page

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -15,6 +15,23 @@ class Admin extends \BaseController {
 		$data->today->users = DAO::count("Utilisateur", "createdAt = NOW()");
 		$data->today->disques = DAO::count("Disque", "createdAt = NOW()");
 
-        $this->loadView('admin/index.html', ["data" => $data]);
+		$this->loadView('admin/index.html', ["data" => $data]);
+	}
+
+	public function users() {
+		$users = DAO::getAll('Utilisateur');
+
+		foreach ($users as $user) {
+			DAO::getOneToMany($user, 'disques');   
+			$user->nbDisques = count($user->getDisques());
+			foreach ($user->getDisques() as $disque) {
+				DAO::getOneToMany($disque, 'disqueTarifs');
+				if ($disque->getTarif() != NULL) {
+					$user->prixTotal = $disque->getTarif()->getPrix();
+				}
+			}
+		}
+
+		$this->loadView('admin/users.html', ["users" => $users]);
 	}
 }
