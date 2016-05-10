23503352137c1f89bf8d500d1d7c18f486ae3c76
21506934 <21506934@etu.unicaen.fr>
2016-05-10 22:44:29 +0200

    Revert "TODO 2.1 : Add new feature"
    This reverts commit c18513b6b52024ebc262745920354b43f7358dfa.

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -3,18 +3,5 @@ use micro\orm\DAO;
 class Admin extends \BaseController {
 
 	public function index() {
-		$data = new stdClass();
-		$data->total = new stdClass();
-		$data->today = new stdClass();
-
-		$data->total->users = DAO::count("Utilisateur");
-		$data->total->disques = DAO::count("Disque");
-		$data->total->tarifs = DAO::count("Tarif");
-		$data->total->services = DAO::count("Service");
-
-		$data->today->users = DAO::count("Utilisateur", "createdAt = NOW()");
-		$data->today->disques = DAO::count("Disque", "createdAt = NOW()");
-
-        $this->loadView('admin/index.html', ["data" => $data]);
 	}
 }
